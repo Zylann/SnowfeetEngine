4b56ebfe175970ed0a36b735dded50bfe237bf29
Marc Gilleron <marc.gilleron@gmail.com>
2014-11-06 23:48:02 +0100

    Added Gource script, for fun

---
+++ gource_view.sh
@@ -0,0 +1,6 @@
+# You need Gource to run this command.
+# Get it here : https://code.google.com/p/gource/
+
+# Displays an animation of a versionned project over time.
+gource -s 5 -i 0 --hide filenames
+
