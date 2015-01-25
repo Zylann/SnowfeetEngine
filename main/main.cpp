c74353522bbb99a74c864f99c28732d918dd66a2
Marc Gilleron <marc.gilleron@gmail.com>
2015-01-25 21:44:33 +0100

    [main] Scoped Application lifetime

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,23 @@
 ﻿#include <iostream>
 //#include <Windows.h>
 #include <core/app/Application.hpp>
+#include <core/system/console/console.hpp>
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Hello Universe!" << std::endl;
-    sn::Application app;
-    return app.execute(sn::CommandLine(argc, argv));
+    int returnCode = 0;
+
+    {
+        std::cout << "Hello Universe!" << std::endl;
+        sn::Application app;
+        returnCode = app.execute(sn::CommandLine(argc, argv));
+    }
+
+#ifdef SN_BUILD_DEBUG
+    sn::pauseConsole();
+#endif
+
+    return returnCode;
 }
 
 //int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpszCmdLine, int nCmdShow)
