887d4a60daf32476b65e140b32d32b990a37fab0
Marc Gilleron <marc.gilleron@gmail.com>
2015-01-25 21:47:34 +0100

    [core][squirrel] Bind tools header

---
+++ core/squirrel/bind_tools.hpp
@@ -0,0 +1,15 @@
+#ifndef __HEADER_SN_SQ_BIND_TOOLS__
+#define __HEADER_SN_SQ_BIND_TOOLS__
+
+#include <squirrel.h>
+//#include <core/util/RefCounted.hpp>
+#include <core/squirrel/sqrat.h>
+
+namespace sn
+{
+
+} // namespace sn
+
+#endif // __HEADER_SN_SQ_BIND_TOOLS__
+
+
