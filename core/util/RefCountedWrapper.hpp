dab70c7d81e2e6a37ecb3b24e0345944d7bc3764
Marc Gilleron <marc.gilleron@gmail.com>
2015-02-01 04:02:15 +0100

    [core][util] Auto-release template pointer for RefCounted instances

---
+++ core/util/RefCountedWrapper.hpp
@@ -0,0 +1,71 @@
+#ifndef __HEADER_SN_REFCOUNTEDWRAPPER__
+#define __HEADER_SN_REFCOUNTEDWRAPPER__
+
+#include <core/util/RefCounted.hpp>
+
+namespace sn
+{
+
+/// \brief Wraps an object inheriting RefCounted and 
+/// automatically addRef() and release() it upon construction and destruction.
+template <class RefCounted_T>
+class RefCountedWrapper
+{
+public:
+	RefCountedWrapper() :
+		m_obj(nullptr)
+	{}
+
+    explicit RefCountedWrapper(RefCounted_T * obj):
+        m_obj(obj)
+    {
+		addRef();
+    }
+
+    RefCountedWrapper(const RefCountedWrapper & other):
+		m_obj(other.m_obj)
+    {
+		addRef();
+	}
+
+    ~RefCountedWrapper()
+    {
+		release();
+	}
+
+	inline bool isNull() const { return m_obj == nullptr; }
+
+    inline RefCounted_T * operator->() { return m_obj; }
+    inline RefCounted_T & operator*() { return *m_obj; }
+
+    inline const RefCounted_T * operator->() const { return m_obj; }
+    inline const RefCounted_T & operator*() const { return *m_obj; }
+
+	inline RefCountedWrapper & operator=(RefCountedWrapper & other)
+	{
+		release();
+		m_obj = other.m_obj;
+		addRef();
+		return *this;
+	}
+
+private:
+	inline void addRef()
+	{
+		if (m_obj)
+			m_obj->addRef();
+	}
+
+	inline void release()
+	{
+		if (m_obj)
+			m_obj->release();
+		m_obj = nullptr;
+	}
+
+    RefCounted_T * m_obj;
+};
+
+} // namespace sn
+
+#endif // __HEADER_SN_REFCOUNTEDWRAPPER__
