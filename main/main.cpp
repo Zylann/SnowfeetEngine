46858e105b9ef49280178dbb392fb6de0c9158f7
Marc Gilleron <marc.gilleron@ubisoft.com>
2015-02-23 14:17:23 +0100

    [main] Test Vector3i hash function: 113 collisions on 1,000,000 iterations. TODO move to Test project.

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,44 @@
 ﻿#include <core/app/appmain.hpp>
 
+#include <core/math/Vector3.hpp>
+#include <core/math/math.hpp>
+#include <map>
+#include <set>
+
+// TODO Put this code into a test project
+void testHashes()
+{
+	std::set<size_t> hashes;
+	std::set<sn::Vector3i> vecs;
+	unsigned int n = 0;
+	unsigned int collisions = 0;
+
+	srand(time(NULL));
+
+	for (unsigned int j = 0; j < 100; ++j)
+	{
+		for (unsigned int i = 0; i < 10000; ++i)
+		{
+			sn::Vector3i v(rand(), rand(), rand());
+			if (vecs.find(v) == vecs.end())
+			{
+				vecs.insert(v);
+				size_t h = sn::getHash(v);
+				auto it = hashes.find(h);
+				if (it != hashes.end())
+					++collisions;
+				else
+					hashes.insert(h);
+				++n;
+			}
+		}
+		std::cout << "Iterations: " << n << ", collisions: " << collisions << std::endl;
+	}
+}
+
 int main(int argc, char * argv[])
 {
+	//testHashes();
     return sn::appMain(argc, argv);
 }
 
