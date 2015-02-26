6add41eae5928a2c804dc257469967c52a28b86c
Marc Gilleron <marc.gilleron@gmail.com>
2015-02-26 02:36:13 +0100

    [gource] Increased animation speed

--- gource_view.sh
+++ gource_view.sh
@@ -1,6 +1,8 @@
 # You need Gource to run this command.
 # Get it here : https://code.google.com/p/gource/
 
+# -s : seconds per day
+
 # Displays an animation of a versionned project over time.
-gource -s 5 -i 0 --hide filenames
+gource -s 1 -i 0 --hide filenames
 
