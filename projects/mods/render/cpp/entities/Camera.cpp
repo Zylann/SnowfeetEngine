f5448bdccd09d4410c32d43830585fad9a9d04f9
Marc Gilleron <marc.gilleron@gmail.com>
2015-03-13 22:14:59 +0100

    [render] Added projection matrix properties to Camera

--- projects/mods/render/cpp/entities/Camera.cpp
+++ projects/mods/render/cpp/entities/Camera.cpp
@@ -6,23 +6,73 @@ namespace render {
 const std::string Camera::TAG = "Camera";
 
 //------------------------------------------------------------------------------
-void Camera::lookAt(Vector3f position)
-{
-    // TODO
-}
-
-//------------------------------------------------------------------------------
 void Camera::setOrtho(bool ortho)
 {
     m_isOrtho = ortho;
-    // TODO
+    m_projectionMatrixNeedUpdate = true;
 }
 
 //------------------------------------------------------------------------------
 void Camera::setOrthoSize(Vector2f orthoSize)
 {
     m_orthoSize = orthoSize;
-    // TODO
+    m_projectionMatrixNeedUpdate = true;
+}
+
+//------------------------------------------------------------------------------
+void Camera::setFov(f32 newFov)
+{
+    m_fov = newFov;
+    m_projectionMatrixNeedUpdate = true;
+}
+
+//------------------------------------------------------------------------------
+void Camera::setNear(f32 newNear)
+{
+    m_near = newNear;
+    m_projectionMatrixNeedUpdate = true;
+}
+
+//------------------------------------------------------------------------------
+void Camera::setFar(f32 newFar)
+{
+    m_far = newFar;
+    m_projectionMatrixNeedUpdate = true;
+}
+
+//------------------------------------------------------------------------------
+void Camera::setAspectRatio(f32 newAspectRatio)
+{
+    m_aspectRatio = newAspectRatio;
+    m_projectionMatrixNeedUpdate = true;
+}
+
+//------------------------------------------------------------------------------
+const Matrix4 & Camera::getProjectionMatrix() const
+{
+    if (m_projectionMatrixNeedUpdate)
+    {
+        if (m_isOrtho)
+        {
+            m_projectionMatrix.loadPerspectiveProjection(
+                math::degToRad(m_fov), 
+                m_aspectRatio, 
+                m_near, m_far
+            );
+        }
+        else
+        {
+            m_projectionMatrix.loadOrtho2DProjection(
+                -m_orthoSize.x(), 
+                m_orthoSize.x(), 
+                m_orthoSize.y()*m_aspectRatio, 
+                m_orthoSize.y()*m_aspectRatio
+            );
+        }
+        m_projectionMatrixNeedUpdate = false;
+    }
+
+    return m_projectionMatrix;
 }
 
 } // namespace render
--- projects/mods/render/cpp/entities/Camera.hpp
+++ projects/mods/render/cpp/entities/Camera.hpp
@@ -1,13 +1,13 @@
 #ifndef __HEADER_SN_RENDER_CAMERA__
 #define __HEADER_SN_RENDER_CAMERA__
 
-#include <core/scene/Entity.hpp>
+#include <core/scene/Entity3D.hpp>
 #include <core/math/Vector2.hpp>
 
 namespace sn {
 namespace render {
 
-class Camera : public Entity
+class Camera : public Entity3D
 {
 public:
 
@@ -16,12 +16,14 @@ public:
     static const std::string TAG;
 
     Camera():
-        Entity(),
+        Entity3D(),
         m_isOrtho(false),
-        m_drawOrder(0)
+        m_fov(70.f),
+        m_near(0.1f),
+        m_far(100.f),
+        m_drawOrder(0),
+        m_projectionMatrixNeedUpdate(true)
     {}
-
-    void lookAt(Vector3f position);
 
     void setOrtho(bool ortho);
     inline bool isOrtho() const { return m_isOrtho; }
@@ -29,7 +31,17 @@ public:
     void setOrthoSize(Vector2f orthoSize);
     inline const Vector2f & getOrthoSize() const { return m_orthoSize; }
 
-    const Matrix4 & getProjectionMatrix() const { return m_projectionMatrix; }
+    inline f32 getFov() const { return m_fov; }
+    inline f32 getNear() const { return m_near; }
+    inline f32 getFar() const { return m_far; }
+    inline f32 getAspectRatio() const { return m_aspectRatio; }
+
+    void setFov(f32 newFov);
+    void setNear(f32 newNear);
+    void setFar(f32 newFar);
+    void setAspectRatio(f32 newAspectRatio);
+
+    const Matrix4 & getProjectionMatrix() const;
 
     inline s32 getDrawOrder() const { return m_drawOrder; }
     inline void setDrawOrder(s32 order) { m_drawOrder = order; }
@@ -41,8 +53,15 @@ private:
     //std::bitset<32> m_cullingMask;
     bool m_isOrtho;
     Vector2f m_orthoSize;
-    Matrix4 m_projectionMatrix;
+    f32 m_fov;
+    f32 m_near;
+    f32 m_far;
+    f32 m_aspectRatio;
     s32 m_drawOrder;
+
+    mutable bool m_projectionMatrixNeedUpdate;
+    mutable Matrix4 m_projectionMatrix;
+
     //Target m_target;
 
 };
