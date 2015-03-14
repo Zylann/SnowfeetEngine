034156dd69b43dfce100c841b46ab6f712bb1c59
Marc Gilleron <marc.gilleron@gmail.com>
2015-03-14 19:28:08 +0100

    [core][math] Implemented getSphericalAngles()

--- core/math/Vector3.hpp
+++ core/math/Vector3.hpp
@@ -19,12 +19,31 @@ typedef Vector<u32, 3> Vector3u;
 typedef Vector<f32, 3> Vector3f;
 
 template <typename T>
-static inline Vector<T, 3> crossProduct(const Vector<T, 3>& v1, const Vector<T, 3>& v2)
+inline Vector<T, 3> crossProduct(const Vector<T, 3>& v1, const Vector<T, 3>& v2)
 {
     return Vector<T, 3>(
         v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2],
         v1[0] * v2[1] - v1[1] * v2[0]
     );
+}
+
+/// \brief Gets spherical coordinates angles (euler angles) of the given vector in degrees.
+/// Assumes the vertical vector to be (0,1,0).
+/// Note: the roll angle will always be zero.
+template <typename T>
+inline Vector<f32, 3> getSphericalAngles(const Vector<T, 3> & v)
+{
+    Vector<f32, 3> angles;
+    f32 lengthSQ = v.getLengthSQ();
+    if (lengthSQ)
+    {
+        if (v.x() != 0)
+            angles.y() = (T)( atan2((f64)v.z(), (f64)v.x()) * math::RAD2DEG64 );
+        else if (v.z() < 0)
+            angles.y() = 180;
+        angles.x() = (T)(acos(v.y() * sqrt(1.0/(lengthSQ))) * math::RAD2DEG64);
+    }
+    return angles;
 }
 
 inline size_t getHash(const sn::Vector3i & v)
