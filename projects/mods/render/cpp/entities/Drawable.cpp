1ba720e2cf94f2da9fd75aad47a4a73824824316
Marc Gilleron <marc.gilleron@ubisoft.com>
2015-03-20 14:05:27 +0100

    [render] Unserialize material and mesh references (Had to hardcode module name, BAD!)

--- projects/mods/render/cpp/entities/Drawable.cpp
+++ projects/mods/render/cpp/entities/Drawable.cpp
@@ -1,4 +1,5 @@
 #include <core/scene/Scene.hpp>
+#include <core/asset/AssetDatabase.hpp>
 #include "Drawable.hpp"
 #include "RenderManager.hpp"
 
@@ -41,6 +42,13 @@ void Drawable::serializeState(JsonBox::Value & o)
 {
     Entity3D::serializeState(o);
     sn::serialize(o["drawOrder"], m_drawOrder);
+	
+	if (!m_material.isNull())
+		sn::serialize(o["material"], m_material.get()->getAssetMetadata().name);
+
+	if (!m_mesh.isNull())
+		sn::serialize(o["mesh"], m_mesh.get()->getAssetMetadata().name);
+
     // TODO
 }
 
@@ -49,6 +57,31 @@ void Drawable::unserializeState(JsonBox::Value & o)
 {
     Entity3D::unserializeState(o);
     sn::unserialize(o["drawOrder"], m_drawOrder);
+
+	std::string materialID;
+	sn::unserialize(o["material"], materialID);
+	if (!materialID.empty())
+	{
+		// TODO Don't hardcode the name of the module!
+		Material * mat = AssetDatabase::get().getAsset<Material>("render", materialID);
+		if (mat)
+		{
+			m_material.set(mat);
+		}
+	}
+
+	std::string meshID;
+	sn::unserialize(o["mesh"], meshID);
+	if (!meshID.empty())
+	{
+		// TODO Don't hardcode the name of the module!
+		Mesh * mesh = AssetDatabase::get().getAsset<Mesh>("render", meshID);
+		if (mesh)
+		{
+			m_mesh.set(mesh);
+		}
+	}
+
     // TODO
 }
 
