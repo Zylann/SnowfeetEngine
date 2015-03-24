533b9a136499e9a6ec7559191a2a11255f212d98
Marc Gilleron <marc.gilleron@gmail.com>
2015-03-24 21:50:36 +0100

    [render] Implemented Texture (not tested yet)

--- projects/mods/render/cpp/Texture.cpp
+++ projects/mods/render/cpp/Texture.cpp
@@ -1,21 +1,29 @@
+#include <core/asset/AssetDatabase.hpp>
+#include <core/asset/base/ImageBase.hpp>
+
+#include "gl_check.hpp"
 #include "Texture.hpp"
 
 namespace sn {
 namespace render {
 
-Texture::Texture() : Asset()
+Texture::Texture() : Asset(),
+    m_textureID(0),
+    //m_samplerID(0),
+    m_isSmooth(false),
+    m_isRepeated(false)
 {
-
 }
 
 Texture::~Texture()
 {
-
+    destroy();
 }
 
 bool Texture::canLoad(const AssetMetadata & meta) const
 {
     // TODO
+    //return AssetDatabase::get().canLoad<ImageBase>();
     return false;
 }
 
@@ -25,6 +33,64 @@ bool Texture::loadFromStream(std::ifstream & ifs)
     return false;
 }
 
+void Texture::destroy()
+{
+    if (m_textureID)
+    {
+        glDeleteTextures(1, &m_textureID);
+        m_textureID = 0;
+    }
+}
+
+bool Texture::create(Vector2u size)
+{
+    return loadFromPixelsRGBA8(size, nullptr);
+}
+
+bool Texture::loadFromPixelsRGBA8(Vector2u size, const char * data)
+{
+    SN_ASSERT(size.x() > 0 && size.y() > 0 && data != nullptr, "Invalid pixel data");
+
+    m_size = size;
+
+    if (m_textureID == 0)
+    {
+        glCheck(glGenTextures(1, &m_textureID));
+        //glGenSamplers(1, &m_samplerID);
+    }
+
+    if (m_textureID)
+    {
+        glCheck(glBindTexture(GL_TEXTURE_2D, m_textureID));
+
+        // Set image data
+        glCheck(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x(), size.y(), 0, GL_RGBA, GL_UNSIGNED_BYTE, data));
+
+        // Set wrapping for X and Y
+        glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_isRepeated ? GL_REPEAT : GL_CLAMP_TO_EDGE));
+        glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_isRepeated ? GL_REPEAT : GL_CLAMP_TO_EDGE));
+
+        // Set upscale and downscale filters
+        glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_isSmooth ? GL_LINEAR : GL_NEAREST));
+        glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_isSmooth ? GL_LINEAR : GL_NEAREST));
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//Texture & Texture::operator=(const Texture & other)
+//{
+//
+//}
+
+void Texture::bind(Texture * tex)
+{
+    glCheck(glBindTexture(GL_TEXTURE_2D, tex->m_textureID));
+}
 
 } // namespace render
 } // namespace sn
--- projects/mods/render/cpp/Texture.hpp
+++ projects/mods/render/cpp/Texture.hpp
@@ -2,22 +2,50 @@
 #define __HEADER_SNR_TEXTURE__
 
 #include <core/asset/Asset.hpp>
+#include <core/math/Vector2.hpp>
+#include <GL/glew.h>
 
 namespace sn {
 namespace render {
 
-class Texture : public Asset
+// TODO Make copyable (needs to download pixels?)
+class Texture : public Asset, public NonCopyable
 {
 public:
     SN_SCRIPT_OBJECT(sn::render::Texture, sn::Asset)
 
     Texture();
 
+    //---------------------------------------
+    // Asset interface
+    //---------------------------------------
+
     bool canLoad(const AssetMetadata & meta) const override;
     bool loadFromStream(std::ifstream & ifs) override;
 
+    //---------------------------------------
+    // Texture interface
+    //---------------------------------------
+
+    void destroy();
+
+    bool create(Vector2u size);
+    bool loadFromPixelsRGBA8(Vector2u size, const char * data);
+
+    //Texture & operator=(const Texture & other);
+
+    static void bind(Texture * tex);
+
 private:
-    ~Texture();
+    ~Texture(); // use release();
+
+private:
+    Vector2u m_size;
+    GLuint m_textureID;
+    bool m_isSmooth;
+    bool m_isRepeated;
+
+    //GLuint m_samplerID;
 
 };
 
