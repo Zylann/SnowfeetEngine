f59ab83aabb1d6c9f0c88175979115f6b6e75b26
Marc Gilleron <marc.gilleron@gmail.com>
2015-04-04 00:48:46 +0200

    [core][math] Fixed wrong signs in Quaternion => Matrix conversion

--- core/math/Matrix4.cpp
+++ core/math/Matrix4.cpp
@@ -205,6 +205,8 @@ void Matrix4::setTranslation(const Vector3f & v)
 //------------------------------------------------------------------------------
 void Matrix4::setRotation(const Quaternion & q)
 {
+#if 0
+    // BROKEN
     // http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/index.htm
 
     //1 - 2 * qy2 - 2 * qz2 	2 * qx*qy - 2 * qz*qw 	2 * qx*qz + 2 * qy*qw
@@ -222,6 +224,21 @@ void Matrix4::setRotation(const Quaternion & q)
     m_v[8 ] =       2.f * q.getX()*q.getZ() - 2.f * q.getY()*q.getW();
     m_v[9 ] =       2.f * q.getY()*q.getZ() + 2.f * q.getX()*q.getW();
     m_v[10] = 1.f - 2.f * q.getX()*q.getX() - 2.f * q.getY()*q.getY();
+#else
+    // Signs have been changed based on Irrlicht's implementation
+
+    m_v[0 ] = 1.f - 2.f * q.getY()*q.getY() - 2.f * q.getZ()*q.getZ();
+    m_v[1 ] =       2.f * q.getX()*q.getY() + 2.f * q.getZ()*q.getW();
+    m_v[2 ] =       2.f * q.getX()*q.getZ() - 2.f * q.getY()*q.getW();
+
+    m_v[4 ] =       2.f * q.getX()*q.getY() - 2.f * q.getZ()*q.getW();
+    m_v[5 ] = 1.f - 2.f * q.getX()*q.getX() - 2.f * q.getZ()*q.getZ();
+    m_v[6 ] =       2.f * q.getY()*q.getZ() + 2.f * q.getX()*q.getW();
+
+    m_v[8 ] =       2.f * q.getX()*q.getZ() + 2.f * q.getY()*q.getW();
+    m_v[9 ] =       2.f * q.getY()*q.getZ() - 2.f * q.getX()*q.getW();
+    m_v[10] = 1.f - 2.f * q.getX()*q.getX() - 2.f * q.getY()*q.getY();
+#endif
 }
 
 //------------------------------------------------------------------------------
