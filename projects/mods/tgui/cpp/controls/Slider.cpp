be1c13086f14969118090e985c76c515523d69a8
Marc Gilleron <marc.gilleron@ubisoft.com>
2015-05-12 13:47:04 +0200

    [tgui] Fixed Slider thumb offset

--- projects/mods/tgui/cpp/controls/Slider.cpp
+++ projects/mods/tgui/cpp/controls/Slider.cpp
@@ -58,10 +58,10 @@ void Slider::onDrawSelf(DrawBatch & batch)
     {
         const auto & thumbTheme = theme->sliderThumbs;
         const IntRect & thumbImageRect = thumbTheme.statesUV[state];
-        s32 x = (m_value * static_cast<f32>(b.width() - thumbImageRect.width())) + thumbImageRect.width() / 2;
+        s32 x = b.minX() + static_cast<s32>(m_value * static_cast<f32>(b.width() - thumbImageRect.width()));
         s32 y = b.minY() + b.height() / 2 - thumbImageRect.height() / 2;
         batch.fillRect(
-            IntRect::fromPositionSize(b.minX() + x, y, thumbImageRect.width(), thumbImageRect.height()),
+            IntRect::fromPositionSize(x, y, thumbImageRect.width(), thumbImageRect.height()),
             thumbImageRect, ts
         );
     }
@@ -86,7 +86,15 @@ void Slider::onMouseMove(Event & ev)
 void Slider::setValueFromPos(sn::Vector2i cursorPos)
 {
     Vector2i size = getSize();
-    m_value = static_cast<f32>(cursorPos[m_orientation]) / static_cast<f32>(size[m_orientation]);
+	s32 padding = 0;
+	const Theme * theme = getTheme();
+	if (theme)
+	{
+		// Apply a padding to the slidable area so we can fix the thumb under the mouse
+		padding = theme->sliderThumbs.statesUV[0].size()[m_orientation] / 2;
+	}
+	f32 rawValue = static_cast<f32>(cursorPos[m_orientation] - padding) / static_cast<f32>(size[m_orientation] - 2 * padding);
+    m_value = math::clamp(rawValue, 0.f, 1.f);
 }
 
 void Slider::serializeState(JsonBox::Value & o, const sn::SerializationContext & ctx)
