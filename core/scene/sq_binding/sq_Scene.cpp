c095380df4479d896912354ca867da37deab9cb4
Marc Gilleron <marc.gilleron@ubisoft.com>
2015-06-19 14:03:33 +0200

    [core][binding] Started binding Entity3D and Scene

--- core/scene/sq_binding/sq_Scene.cpp
+++ core/scene/sq_binding/sq_Scene.cpp
@@ -5,23 +5,61 @@ This file is part of the SnowfeetEngine project.
 */
 
 #include "sq_scene.hpp"
+#include "../../squirrel/bind_macros.h"
 
 namespace sn
 {
 
+	namespace
+	{
+		DEFINE_CLASS(Scene)
+
+		BEGIN_METHOD(getTaggedEntity)
+			const char * tagName = getString(vm, 2);
+			Entity * e = self->getTaggedEntity(tagName);
+			if (e)
+				e->pushScriptObject(vm);
+			else
+				sq_pushnull(vm);
+			return 0;
+		END_METHOD
+
+		BEGIN_METHOD(getTaggedEntities)
+			const char * tagName = getString(vm, 2);
+			std::vector<Entity*> entities = self->getTaggedEntities(tagName);
+			sq_newarray(vm, entities.size());
+			for (u32 i = 0; i < entities.size(); ++i)
+			{
+				sq_pushinteger(vm, i);
+				Entity * e = entities[i];
+				e->pushScriptObject(vm);
+				sq_set(vm, -3);
+			}
+			return 1;
+			END_METHOD
+
+		BEGIN_METHOD(getDeltaTime)
+			Time t = self->getDeltaTime();
+			sq_pushfloat(vm, t.asSeconds());
+			return 1;
+		END_METHOD
+
+		BEGIN_METHOD(getTimeSinceStartup)
+			Time t = self->getTimeSinceStartup();
+			sq_pushfloat(vm, t.asSeconds());
+			return 1;
+		END_METHOD
+
+	}
+
+
 void registerScene(HSQUIRRELVM vm)
 {
-    // TODO FIXME The following doesn't compiles yet...
-
-    //using namespace Sqrat;
-
-    //const char * className = "Scene";
-
-    //DerivedClass<Scene,Entity,RefCountedAllocator<Scene>> c(vm, className);
-
-    //c.Func("quit", &Scene::quit);
-
-    //RootTable(vm).Bind(className, c);
+	ScriptableObject::bindBase<Scene>(vm)
+		.setMethod("getTaggedEntity", getTaggedEntity)
+		.setMethod("getTaggedEntities", getTaggedEntities)
+		.setMethod("getDeltaTime", getDeltaTime)
+		.setMethod("getTimeSinceStartup", getTimeSinceStartup);
 }
 
 } // namespace sn
