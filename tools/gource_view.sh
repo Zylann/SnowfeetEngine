f637bbb7ba8fad3320e9e04d2450bd8f47109b87
Marc Gilleron <marc.gilleron@gmail.com>
2015-07-05 04:34:01 +0200

    [tools] Faster Gource animation + hide big libraries

--- tools/gource_view.sh
+++ tools/gource_view.sh
@@ -4,5 +4,4 @@
 # -s : seconds per day
 
 # Displays an animation of a versionned project over time.
-gource -s 1 -i 0 --hide filenames
-
+gource -s 0.2 -i 0 --hide filenames --max-user-speed 1000 --file-filter "\bexternal\b|\bassimp\b|\bfreetype\b"
