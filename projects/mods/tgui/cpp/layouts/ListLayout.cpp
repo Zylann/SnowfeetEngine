a8fcb3fe39f48f6ea3cf6f4d66073ff87ecf847e
Marc Gilleron <marc.gilleron@ubisoft.com>
2015-09-10 14:03:40 +0200

    [tgui] Horizontal list layout support (but direction would be better actually!)

--- projects/mods/tgui/cpp/layouts/ListLayout.cpp
+++ projects/mods/tgui/cpp/layouts/ListLayout.cpp
@@ -32,19 +32,11 @@ void ListLayout::update()
 	Control * container = getContainer();
 	SN_ASSERT(container != nullptr, "ListLayout container is null!");
 
-    if (m_orientation == TGUI_HORIZONTAL)
-    {
-        SN_WARNING("Vertical ListLayout orientation is not implemented yet");
-        return;
-    }
-
 	// TODO Have a dirty flag to avoid calculating layouts when nothing changed
 
     std::vector<Control*> children;
 	container->getChildrenOfType<Control>(children);
 
-    // Default vertical layout
-    
 	// Get container bounds with padding
 	sn::IntRect localBounds = container->getLocalClientBounds();
 	localBounds.x() = 0;
@@ -53,34 +45,98 @@ void ListLayout::update()
 
 	IntRect cellBounds;
 	cellBounds.origin() = localBounds.origin();
-	cellBounds.width() = localBounds.width();
 
-	for (auto it = children.begin(); it != children.end(); ++it)
-    {
-        Control & child = **it;
-        if (child.getPositionMode() == TGUI_LAYOUT)
-        {
-			const Border & margin = child.getMargin();
-            IntRect childBounds = child.getLocalClientBounds();
+	if (m_orientation == TGUI_VERTICAL)
+	{
+		cellBounds.width() = localBounds.width();
 
-			childBounds.origin() = cellBounds.origin();
+		for (auto it = children.begin(); it != children.end(); ++it)
+		{
+			Control & child = **it;
+			if (child.getPositionMode() == TGUI_LAYOUT)
+			{
+				const Border & margin = child.getMargin();
+				IntRect childBounds = child.getLocalClientBounds();
 
-			// Anchors
-			cellBounds.height() = childBounds.height() + margin.top + margin.bottom;
-			applyAnchors(childBounds, cellBounds, child.getAnchors());
+				childBounds.origin() = cellBounds.origin();
 
-			// TODO lastChildFill property
+				// Anchors
+				cellBounds.height() = childBounds.height() + margin.top + margin.bottom;
+				applyAnchors(childBounds, cellBounds, child.getAnchors());
 
-			// Margin
-			child.getMargin().crop(childBounds);
+				// Margin
+				child.getMargin().crop(childBounds);
 
-			// Apply
-            child.setLocalClientBounds(childBounds);
-            child.layoutChildren();
+				// Apply
+				child.setLocalClientBounds(childBounds);
+				child.layoutChildren();
 
-			cellBounds.y() += cellBounds.height() + m_spacing;
+				cellBounds.y() += cellBounds.height() + m_spacing;
+			}
 		}
-    }
+	}
+	else
+	{
+		cellBounds.height() = localBounds.height();
+
+		for (auto it = children.begin(); it != children.end(); ++it)
+		{
+			Control & child = **it;
+			if (child.getPositionMode() == TGUI_LAYOUT)
+			{
+				const Border & margin = child.getMargin();
+				IntRect childBounds = child.getLocalClientBounds();
+
+				childBounds.origin() = cellBounds.origin();
+
+				// Anchors
+				cellBounds.width() = childBounds.width() + margin.left + margin.right;
+				applyAnchors(childBounds, cellBounds, child.getAnchors());
+
+				// Margin
+				child.getMargin().crop(childBounds);
+
+				// Apply
+				child.setLocalClientBounds(childBounds);
+				child.layoutChildren();
+
+				cellBounds.x() += cellBounds.width() + m_spacing;
+			}
+		}
+	}
+
+	// TODO lastChildFill parameter?
+
+	// TODO Proposal?
+
+	//unsigned int o = m_orientation;
+
+	//cellBounds.size()[o] = localBounds.size()[o];
+
+	//for (auto it = children.begin(); it != children.end(); ++it)
+	//{
+	//	Control & child = **it;
+	//	if (child.getPositionMode() == TGUI_LAYOUT)
+	//	{
+	//		const Border & margin = child.getMargin();
+	//		IntRect childBounds = child.getLocalClientBounds();
+
+	//		childBounds.origin() = cellBounds.origin();
+
+	//		// Anchors
+	//		cellBounds.size()[o] = childBounds.size()[o] + (o == TGUI_HORIZONTAL ? (margin.left + margin.right) : (margin.top + margin.bottom));
+	//		applyAnchors(childBounds, cellBounds, child.getAnchors());
+
+	//		// Margin
+	//		child.getMargin().crop(childBounds);
+
+	//		// Apply
+	//		child.setLocalClientBounds(childBounds);
+	//		child.layoutChildren();
+
+	//		cellBounds.origin()[o] += cellBounds.size()[o] + m_spacing;
+	//	}
+	//}
 }
 
 //------------------------------------------------------------------------------
