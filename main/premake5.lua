d5dfa5da4e7aab41d21cdcc2f4cdea3b5b84fa2f
Marc Gilleron <marc.gilleron@gmail.com>
2015-10-25 01:25:34 +0200

    [core] Fixed execution directory

--- main/premake5.lua
+++ main/premake5.lua
@@ -14,8 +14,9 @@ project "SnowfeetMain"
 	}
 	filter "configurations:Debug"
 		targetdir "../_bin/debug"
+		debugdir "../_bin/debug"
 		objdir "../_obj/debug"
 	filter "configurations:Release"
 		targetdir "../_bin/release"
+		debugdir "../_bin/release"
 		objdir "../_obj/release"
-
