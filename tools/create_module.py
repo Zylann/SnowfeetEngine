f7bac049c474e0efeb852fca695e185188e63780
Marc Gilleron <marc.gilleron@gmail.com>
2015-10-27 02:00:27 +0100

    [tools] Fixed and simplified create_module.py (module refacto)

--- tools/create_module.py
+++ tools/create_module.py
@@ -4,33 +4,24 @@
 import sys, os, json
 
 
-# mod_types = {
-# 	"mod":"Mod",
-# 	"sample":"Sample",
-# 	"usermod":"UserMod",
-# 	"tool":"Tool"
-# }
-
 header_extension = ".h"
 
 
-def get_mod_lib_name(modType, name):
-	return modType.title() + name.title()
+def get_mod_lib_name(name):
+	return "Mod" + name.title()
 
 
-def create_cpp_folder(root, modType, namespace, name):
-	cppDir = os.path.join(root, "cpp")
-	os.mkdir(cppDir)
-
+def create_cpp_folder(root, namespace, name):
+	cppDir = root
 	eol = "\n"
 	indent = "    "
 
-	headerName = "mod_" + name.title() + header_extension
+	headerName = "Module" + header_extension
 	headerPath = os.path.join(cppDir, headerName)
-	implemPath = os.path.join(cppDir, "mod_" + name.title() + ".cpp")
+	implemPath = os.path.join(cppDir, "Module" + ".cpp")
 	premakePath = os.path.join(cppDir, "premake5.lua")
 
-	libName = get_mod_lib_name(modType, name)
+	libName = get_mod_lib_name(name)
 	loadFunc = "int loadSnowfeetModule_" + libName + "(ModuleLoadArgs args)"
 	unloadFunc = "int unloadSnowfeetModule_" + libName + "(ModuleUnloadArgs args)"
 	regFuncName = "registerObjectTypes"
@@ -92,58 +83,26 @@ def create_cpp_folder(root, modType, namespace, name):
 
 	f = open(premakePath, "w+")
 	f.write(eol.join([
-		"project \"" + get_mod_lib_name(modType, name) + "\"",
-		indent + "platforms { \"x32\" }",
+		"project \"" + libName + "\"",
 		indent + "commonModConfigCPP()",
-		#indent + "kind \"SharedLib\"",
-		#indent + "language \"C++\"",
-		#indent + "dependson {",
-		#indent + indent + "\"SnowfeetCore\"",
-		#indent + indent + "\"JsonBox\"",
-		#indent + "}",
-		#indent + "location \".\"",
-		#indent + "targetdir \"..\"",
 		indent + "files {",
 		indent + indent + "\"**" + header_extension + "\",",
 		indent + indent + "\"**.cpp\"",
 		indent + "}",
-		#indent + "links {",
-		#indent + indent + "\"SnowfeetCore\",",
-		#indent + indent + "\"JsonBox\" -- TODO Don't link JsonBox, it's already done in the core (maybe a premake5 bug?)",
-		#indent + "}",
-		indent + "filter \"configurations:Debug\"",
-		indent + indent + "objdir \"_obj/debug\"",
-		#indent + indent + "defines {",
-		#indent + indent + indent + "\"SN_BUILD_DEBUG\"",
-		#indent + indent + "}",
-		indent + "filter \"configurations:Release\"",
-		indent + indent + "objdir \"_obj/release\"",
+		# indent + "filter \"configurations:Debug\"",
+		# indent + indent + "objdir \"_obj/debug\"",
+		# indent + "filter \"configurations:Release\"",
+		# indent + indent + "objdir \"_obj/release\"",
 		""
 	]))
 	f.close()
 
 
-def create_mod_file(path, modType, name):
-	f = open(path, "w+")
-	contents = {
-		"bindings":[
-			get_mod_lib_name(modType, name)
-		]
-	}
-	f.write(json.dumps(contents, sort_keys=True, indent=4, separators=(',', ': ')))
-	f.close()
-	return contents
-
-
-def create_module(root, modType, name, cppNamespace):
-	modDir = os.path.join(root, modType + "s", name.lower())
+def create_module(root, name, cppNamespace):
+	modDir = os.path.join(root, name.lower())
 	if not os.path.isdir(modDir):
 		os.makedirs(modDir)
-	
-	modFilePath = os.path.join(modDir, name.lower() + ".mod.json")
-	modContents = create_mod_file(modFilePath, modType, name)
-
-	create_cpp_folder(modDir, modType, cppNamespace, name)
+	create_cpp_folder(modDir, cppNamespace, name)
 
 
 def print_usage():
@@ -156,7 +115,7 @@ def main():
 		cppNamespace = "sn"
 		if len(sys.argv) == 3:
 			cppNamespace = sys.argv[2]
-		create_module("projects", "mod", modName, cppNamespace)
+		create_module("modules", modName, cppNamespace)
 	else:
 		print_usage()
 
@@ -165,4 +124,3 @@ def main():
 os.chdir('..')
 
 main()
-
