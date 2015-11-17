67bc9765410a5a0b99a39b618ce457cfd34ba1e6
Marc Gilleron <marc.gilleron@gmail.com>
2015-11-17 02:41:01 +0100

    [tgui][binding] Added Control.isPressed()

--- modules/tgui/bind/sq_Control.cpp
+++ modules/tgui/bind/sq_Control.cpp
@@ -7,11 +7,20 @@ namespace tgui
     namespace
     {
         CURRENT_CLASS(Control)
+
+        SQRESULT isPressed(HSQUIRRELVM vm)
+        {
+            GET_SELF();
+            sq_pushbool(vm, self->isPressed());
+            return 1;
+        }
     }
 
 void bindControl(HSQUIRRELVM vm)
 {
-    sn::ScriptableObject::bindBase<Control>(vm);
+    sn::ScriptableObject::bindBase<Control>(vm)
+        .setMethod("isPressed", isPressed)
+    ;
 }
 
 } // namespace tgui
